b254c0396a96a752dbb9f9943aaa076f104840c9
PedroPradoCho <pedro.araujo@sptech.school>
2023-04-25 12:01:45 -0300

    Adicionando verificação de login + modal de login iválido

--- site/public/javascript/cadastro/cliente.js
+++ site/public/javascript/cadastro/cliente.js
@@ -1,26 +1,32 @@
-function login(){
-    var email = input_email.value;
-    var senha = input_senha.value;
+function login() {
+    var emailLogin = document.querySelector('#input-email-login').value;
+    var senhaLogin = document.querySelector('#input-senha-login').value;
+    var modalShow = document.getElementById('modal-container')
 
-    if(senha == "123" && email == "healthyfish@sptech.com"){
-        alert("Login com sucesso!, acesso ao dashboard")
+    if (senhaLogin == "123" && emailLogin == "healthyfish@sptech.com") {
+        window.location.href = "../../ChartJs/dashboard.html";
     } else {
-        alert("Login ou senha incorretos!")
+        modalShow.style.display = "flex"
     }
 }
 
-function cadastro(){
+function cadastro() {
     var cpf = Number(input_cpf.value);
     var cnpj = Number(input_cnpj.value);
 
-    if(cpf.length == 11){
+    if (cpf.length == 11) {
         console.log("cpf")
         //campos para cadastro de cpf
-    } else if(cnpj.length == 14){
+    } else if (cnpj.length == 14) {
         console.log("cnpj")
         //capos para cadastro de cnpj
     } else {
         console.log("erro")
         // dados incorretos
     }
+}
+
+function disableModal() {
+    var modalShow = document.getElementById('modal-container')
+    modalShow.style.display = "none"
 }
